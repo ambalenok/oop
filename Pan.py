1606787790ae1998e2d70c5e0abe30182ea2989c
♥ыыы)♥ <ambal.vpn@gmail.com>
2022-03-11 17:21:18 +0400

    - todo

---
+++ Pan.py
@@ -0,0 +1,87 @@
+import os.path
+import json
+
+from Patients import Patients
+from Doctor import Doctors
+from Appeals import Appeals
+from ClassList import ClassList
+
+
+class Pan(object):
+    def __init__(self, file_name=""):
+        # todo заменить ClassList() на базовый объект list()
+        self.__patientse = ClassList()
+        self.__doctorse = ClassList()
+        self.__appealse = ClassList()
+        if file_name:
+            self.read(file_name)
+
+    # todo Вынести данный метод в соответствующие контроллеры
+    def read(self, file_name):
+        with open(file_name) as file_input:
+            data = json.load(file_input)
+
+        for item in data["doctors"]:
+            doctors = Doctors(item["id"], item["surname"], item["patronymic"], item["specialization"], item["category"])
+        self.__doctorse.append(doctors)
+
+        for item in data["patients"]:
+            patients = Patients(item["id"], item["surname"], item["patronymic"], item["year"])
+        self.__patientse.append(patients)
+
+        for item in data["appeals"]:
+            appeals = Appeals(item["id"], self.__doctorse.getByID(item["doctors"]),
+                              self.__patientse.getByID(item["patients"]),
+                              item["date"], item["diagnos"], item["cost"])
+            self.__appealse.append(appeals)
+
+    # todo Вынести данный метод в соответствующие контроллеры
+    def write(self, file_name):
+        if not os.path.exists(file_name):
+            data = {}
+        else:
+            with open(file_name) as file_input:
+                data = json.load(file_input)
+
+        data["doctors"] = []
+        for item in self.__doctorse:
+            doctors = {
+                "id": item.getId(),
+                "surname": item.getSurname(),
+                "patronymic": item.getPatronymic(),
+                "specialization": item.getSpecialization(),
+                "category": item.getCategory()
+            }
+            data["doctors"].append(doctors)
+
+        data["patients"] = []
+        for item in self.__patientse:
+            patients = {
+                "id": item.getID(),
+                "surname": item.getSurname(),
+                "patronymic": item.getPatronymic(),
+                "year": item.getYear(),
+            }
+            data["patients"].append(patients)
+
+        data["appeals"] = []
+        for item in self.__appealse:
+            appeal = {
+                "id": item.getId(),
+                "doctorse": item.getDoctors(),
+                "patientse": item.getPatients(),
+                "date": item.getDate(),
+                "diagnos": item.getDiagnos(),
+                "cost": item.getCost()
+            }
+            data["appeals"].append(appeal)
+        with open(file_name, "w") as file_output:
+            json.dump(data, file_output)
+
+    def print(self):
+        for item in self.__doctorse, self.__patientse, self.__appealse:
+            print(item)
+
+
+x = Pan("data.json")
+x.print()
---
+++ Patients.py
@@ -0,0 +1,17 @@
+from Shared import Shared
+
+
+class Patients(Shared):
+    def __init__(self, id, surname, patronymic, year):
+        super().__init__(id, surname, patronymic)
+        self.__year = year
+
+    def setYear(self, year):
+        self.__year = year
+
+    def getYear(self):
+        return self.__year
+
+    def __repr__(self):
+        return (f"ID: {self.getID()}\n Имя пациента: {self.getSurname()}\n Фамилия: {self.getPatronymic()}\n "
+                f"Лет:{self.__year}")
