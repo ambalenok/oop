d95922c840ca99dbdf55353a04b123f6d257deca
♥ыыы)♥ <ambal.vpn@gmail.com>
2022-03-31 18:28:41 +0400

    there are no controllers yet

--- src/Pan.py
+++ src/Pan.py
@@ -1,85 +1,13 @@
-import os.path
-import json
-
-from model.Patients import Patients
-from model.Doctor import Doctors
-from model.Appeals import Appeals
-from service.ClassListService import ClassListService
+from controllers.ControllerMet import ControllerMet
 
 
 class Pan(object):
     def __init__(self, file_name=""):
-
         self.__patientse = list()
         self.__doctorse = list()
         self.__appealse = list()
         if file_name:
-            self.read(file_name)
-
-    # todo Вынести данный метод в соответствующие контроллеры
-    def read(self, file_name):
-        with open(file_name) as file_input:
-            data: dict = json.load(file_input)
-
-        doctors = data.get("doctors")
-
-        for item in doctors:
-            doctor = Doctors(item["id"], item["surname"], item["patronymic"], item["specialization"], item["category"])
-            self.__doctorse.append(doctor)
-
-        patients = data.get("patients")
-        for item in patients:
-            patients = Patients(item["id"], item["surname"], item["patronymic"], item["year"])
-        self.__patientse.append(patients)
-
-        for item in data["appeals"]:
-            appeals = Appeals(item["id"], ClassListService.getByID(self.__doctorse, item["doctors"]),
-                              ClassListService.getByID(self.__patientse,item["patients"]),
-                              item["date"], item["diagnos"], item["cost"])
-            self.__appealse.append(appeals)
-
-    # todo Вынести данный метод в соответствующие контроллеры
-    def write(self, file_name):
-        if os.path.exists(file_name):
-            with open(file_name) as file_input:
-                data = json.load(file_input)
-        else:
-            data = dict()
-
-        data["doctors"] = []
-        for item in self.__doctorse:
-            doctors = {
-                "id": item.getId(),
-                "surname": item.getSurname(),
-                "patronymic": item.getPatronymic(),
-                "specialization": item.getSpecialization(),
-                "category": item.getCategory()
-            }
-            data["doctors"].append(doctors)
-
-        data["patients"] = []
-        for item in self.__patientse:
-            patients = {
-                "id": item.getID(),
-                "surname": item.getSurname(),
-                "patronymic": item.getPatronymic(),
-                "year": item.getYear(),
-            }
-            data["patients"].append(patients)
-
-        data["appeals"] = []
-        for item in self.__appealse:
-            appeal = {
-                "id": item.getId(),
-                "doctorse": item.getDoctors(),
-                "patientse": item.getPatients(),
-                "date": item.getDate(),
-                "diagnos": item.getDiagnos(),
-                "cost": item.getCost()
-            }
-            data["appeals"].append(appeal)
-        with open(file_name, "w") as file_output:
-            json.dump(data, file_output)
+            ControllerMet.read(file_name, self.__patientse, self.__doctorse, self.__appealse)
 
     def print(self):
         for item in self.__doctorse, self.__patientse, self.__appealse:
--- src/controllers/ControllerMet.py
+++ src/controllers/ControllerMet.py
@@ -1,13 +1,74 @@
+import json
+import os.path
+from model.Patients import Patients
+from model.Doctor import Doctors
+from model.Appeals import Appeals
+from service.ClassListService import ClassListService
+
+
 class ControllerMet(object):
 
-    def create(self):
-        pass
+    def read(file_name, doctorse, patientse, appealse):
+        with open(file_name) as file_input:
+            data: dict = json.load(file_input)
+        doctors = data.get("doctors")
 
-    def read(self):
-        pass
+        for item in doctors:
+            doctor = Doctors(item["id"], item["surname"], item["patronymic"], item["specialization"], item["category"])
+            doctorse.append(doctor)
 
-    def update(self):
-        pass
+        patients = data.get("patients")
+        for item in patients:
+            patients = Patients(item["id"], item["surname"], item["patronymic"], item["year"])
+        patientse.append(patients)
+
+        for item in data["appeals"]:
+            appeals = Appeals(item["id"], ClassListService.getByID(doctorse, item["doctors"]),
+                              ClassListService.getByID(patientse, item["patients"]),
+                              item["date"], item["diagnos"], item["cost"])
+            appealse.append(appeals)
+
+    def write(file_name, doctorse, patientse, appealse):
+        if os.path.exists(file_name):
+            with open(file_name) as file_input:
+                data = json.load(file_input)
+        else:
+            data = dict()
+
+        data["doctors"] = []
+        for item in doctorse:
+            doctors = {
+                "id": item.getId(),
+                "surname": item.getSurname(),
+                "patronymic": item.getPatronymic(),
+                "specialization": item.getSpecialization(),
+                "category": item.getCategory()
+            }
+            data["doctors"].append(doctors)
+
+        data["patients"] = []
+        for item in patientse:
+            patients = {
+                "id": item.getID(),
+                "surname": item.getSurname(),
+                "patronymic": item.getPatronymic(),
+                "year": item.getYear(),
+            }
+            data["patients"].append(patients)
+
+        data["appeals"] = []
+        for item in appealse:
+            appeal = {
+                "id": item.getId(),
+                "doctorse": item.getDoctors(),
+                "patientse": item.getPatients(),
+                "date": item.getDate(),
+                "diagnos": item.getDiagnos(),
+                "cost": item.getCost()
+            }
+            data["appeals"].append(appeal)
+        with open(file_name, "w") as file_output:
+            json.dump(data, file_output)
 
     def delete(self):
         pass
