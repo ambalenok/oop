5f1ce6dc0ca3c98272b541f00d4bc460aa470145
Ыыы) <ambal.vpn@gmail.com>
2022-04-06 01:12:11 +0400

    test

--- src/service/ClassListService.py
+++ src/service/ClassListService.py
@@ -1,11 +1,12 @@
 class ClassListService(object):
-    # todo переименуй self на более понятное название.
-    # todo Следи за названиями переменных. По их названию должно быть понятно, что в них находиться
+
     @staticmethod
-    def getByID(self, id):
+    def getByID(classlist, id):
         # todo цикл работает не корректно, сейчас он проверяет только первый элемент в списке и дальше не идёт
         # todo разберись почему и исправь ошибку
-        for item in self:
-            if item.getID() == id:
-                return item
-            return None
+
+        while len(classlist):
+            for item in classlist:
+                if item.getID() == id:
+                    return item
+                return None
