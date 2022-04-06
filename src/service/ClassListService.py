c56e17d265e28632bf5e9efc96aaba227c228595
♥ыыы)♥ <ambal.vpn@gmail.com>
2022-04-06 16:03:16 +0400

    test 2

--- src/service/ClassListService.py
+++ src/service/ClassListService.py
@@ -2,11 +2,8 @@ class ClassListService(object):
 
     @staticmethod
     def getByID(classlist, id):
-        # todo цикл работает не корректно, сейчас он проверяет только первый элемент в списке и дальше не идёт
-        # todo разберись почему и исправь ошибку
 
-        while len(classlist):
-            for item in classlist:
-                if item.getID() == id:
-                    return item
-                return None
+        for item in classlist:
+            if item.getID() == id:
+                item += item
+        return item
