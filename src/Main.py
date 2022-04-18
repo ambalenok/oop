41bbc1a7861a600d946aa04ec31d58e9c35b0dd5
Ыыы) <ambal.vpn@gmail.com>
2022-04-18 10:31:14 +0400

    add dict and edits in the main file

--- src/Main.py
+++ src/Main.py
@@ -1,19 +1,10 @@
-from controllers.ControllerDoc import ControllerDoc
-from controllers.ControllerPat import ControllerPat
-from controllers.ControllerApp import ControllerApp
+from controllers.Handling import Handling
 
 
 class Main(object):
     def __init__(self):
-        controllerPat = ControllerPat()
-        controllerDoc = ControllerDoc()
-        controllerApp = ControllerApp()
-
-        patientse = controllerPat.read()
-        doctorse = controllerDoc.read()
-        appealse = controllerApp.read(patientse, doctorse)
-
-        self.__list = patientse + doctorse + appealse
+        handling = Handling()
+        self.__list = handling.read()
 
     def read(self):
         for item in self.__list:
---
+++ src/controllers/Handling.py
@@ -0,0 +1,17 @@
+from controllers.ControllerDoc import ControllerDoc
+from controllers.ControllerPat import ControllerPat
+from controllers.ControllerApp import ControllerApp
+from controllers.BasiController import BasiController
+
+
+class Handling(BasiController):
+    def read(self):
+        controllerPat = ControllerPat()
+        controllerDoc = ControllerDoc()
+        controllerApp = ControllerApp()
+
+        patientse = controllerPat.read()
+        doctorse = controllerDoc.read()
+        appealse = controllerApp.read(patientse, doctorse)
+        self.__list = patientse + doctorse + appealse
+        return self.__list
